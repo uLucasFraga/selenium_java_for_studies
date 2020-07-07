5ae2e338af579e4aad4e1f4787567962b4bbd0b8
lucas fraga <lucass.fragaa@gmail.com>
2020-07-07 15:40:04 -0300

    testing v1.8 - actions

--- src/test/java/br/com/studies/utils/DriverFactory.java
+++ src/test/java/br/com/studies/utils/DriverFactory.java
@@ -18,7 +18,6 @@ public class DriverFactory {
       ChromeOptions options = new ChromeOptions();
       options.addArguments(
           "window-size=1920,1080",
-//          "--start-maximized",
           "--disable-dev-shm-usage",
           "--disable-gpu",
           "--no-sandbox");
